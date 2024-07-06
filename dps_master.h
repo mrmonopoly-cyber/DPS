c5de4e0dafe01188785c690a0f49a70587dd8c39
Alberto Damo <alberto.damo@gmail.com>
2024-07-06 11:55:30 +0200

    fixed contract master

--- dps_master.h
+++ dps_master.h
@@ -17,10 +17,10 @@ void dps_master_refresh();
 const c_vector* dps_master_board_list();
 
 //INFO: send a mex to a specific board to updated a variable of the board
-void dps_master_update(const uint8_t board_id, const uint8_t data_id, const char* var_name);
+void dps_master_update(const uint8_t board_id, const uint8_t data_id, const void* value);
 
 //INFO: send a command in the system with a given value
-void dps_master_send_command(const can_id id_comm, const uint8_t value);
+void dps_master_send_command(const can_id id_comm, const void* value);
 
 //INFO: check if a given can message is of the belong to the library
 //if it's the case it use it and return 1
