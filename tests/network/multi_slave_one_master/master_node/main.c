13f6b481fc00978319e8992a21b6eac95e494a08
Alberto Damo <alberto.damo@gmail.com>
2024-08-13 09:31:51 +0200

    prepared init tests multi slaves one master

--- tests/network/multi_slave_one_master/master_node/main.c
+++ tests/network/multi_slave_one_master/master_node/main.c
@@ -7,28 +7,26 @@
 
 int can_socket = -1;
 
-uint8_t send_f_can(can_message* mex){
+int send_f_can(CanMessage* mex){
     struct can_frame f = {
-        .can_dlc = mex->mex_size,
-        .can_id = mex->id.full_id,
+        .can_dlc = mex->dlc,
+        .can_id = mex->id,
     };
-    memcpy(f.data, mex->data,mex->mex_size);
+    memcpy(f.data, mex->rawMex.raw_buffer,mex->dlc);
     return can_send_frame(can_socket, &f);
 }
 
 void* check_incomming_message(void* args){
     struct can_frame mex_lib = {0};
-    can_message mex = {0};
     while(1){
         if(!can_recv_frame(can_socket, &mex_lib)){
-            mex.mex_size = mex_lib.can_dlc;
-            mex.id.full_id = mex_lib.can_id;
-            memcpy(mex.data, mex_lib.data, mex_lib.can_dlc);
-            if(dps_master_check_can_mex_recv(&mex)){
-                fprintf(stderr,"dps master receive a message\n");
-            }
+            CanMessage mex = {
+                .dlc = mex_lib.can_dlc,
+                .id = mex_lib.can_id,
+            };
+            memcpy(mex.rawMex.raw_buffer, mex_lib.data, mex_lib.can_dlc);
+            dps_master_check_mex_recv(&mex);
         }
-        fprintf(stderr,"receive a message\n");
     };
     return NULL;
 }
@@ -45,18 +43,6 @@ int main(void)
     pthread_t new_thread = 1;
     pthread_create(&new_thread, NULL, check_incomming_message, NULL);
     
-    fprintf(stderr,"requiring info from slave\n");
-    dps_master_refresh();
-
-    char buff[10] = {0};
-    while(strcmp(buff, "HELLO")){
-        memset(buff, 0, sizeof(buff));
-        scanf("%s",buff);
-    }
-
-    fprintf(stderr,"printing info board\n ");
-    dps_master_print_board();
-    fprintf(stderr,"done printing\n ");
 
     pthread_join(new_thread, NULL);
     return 0;
